376b3350688978e68f17ecd337de0bf3af2d48d8
hychua <85439265+hychua@users.noreply.github.com>
2021-06-08 15:39:22 +0800

    Add files via upload

--- IE-253-eQuipped-Chua.py
+++ IE-253-eQuipped-Chua.py
@@ -125,6 +125,13 @@ encoded_image = base64.b64encode(open(image_filename, 'rb').read())
 image_filename2 = 'equipped-icon.jpg' # replace with your own image
 encoded_image2 = base64.b64encode(open(image_filename2, 'rb').read())
 
+order_icon = 'order_icon.png' # replace with your own image
+order_encoded_image = base64.b64encode(open(order_icon, 'rb').read())
+equi_icon = 'equi_icon.png' # replace with your own image
+equi_encoded_image = base64.b64encode(open(equi_icon, 'rb').read())
+report_icon = 'report_icon.png' # replace with your own image
+report_encoded_image = base64.b64encode(open(report_icon, 'rb').read())
+
 url_bar_and_content_div = html.Div([
     dcc.Location(id='url', refresh=False),
     html.Div(id='page-content')
@@ -144,15 +151,23 @@ layout_index = html.Div([
                         ],
                         style={'display':'inline-block','margin-left':50}),
                 
-            dcc.Link("Need to manage User Account Access? Click Here!", href='/register',
+            dcc.Link("Need to Manage User Access? Click Here!", href='/register',
                      style={'font-family':'avenir','fontSize':16,'display':'inline-block','float':'right'}),
             
     html.Div(' ',
         style={'backgroundColor':'rgb(0,123,255)','height':20,'borderRadius':5}),
+    html.Button(
+                        id='dummy-button',
+                        n_clicks=0,
+                        children='Dummy',
+                        style={'fontSize':18,
+                               'color':'rgb(0,123,255)',
+                               'backgroundColor':'rgb(255,255,255)',
+                               'borderRadius':5,
+                               'height':42,'width':200,
+                               'font-family':'minion', 'float':'right',
+                               'display':'none'}),    
         
-                
-            
-    
     html.Div([
         html.H2('Which type of data do you need to manage?',
                             style={'color':'rgb(0,123,255)',
@@ -160,6 +175,9 @@ layout_index = html.Div([
         
         html.Div([
         html.Div([
+            html.Img(src='data:image/png;base64,{}'.format(order_encoded_image.decode()),
+                     style={'width':100}),
+            html.Br(),            
             html.H3('Transactional Data',
                     style={'color':'rgb(0,123,255)','textAlign':'center',
                            'font-family':'avenir'}),
@@ -191,6 +209,9 @@ layout_index = html.Div([
             style={'display':'inline-block'}),
         
         html.Div([
+            html.Img(src='data:image/png;base64,{}'.format(equi_encoded_image.decode()),
+                     style={'width':100}),
+            html.Br(),
             html.H3('Master Data',
                     style={'color':'rgb(0,123,255)','textAlign':'center',
                            'font-family':'avenir'}),
@@ -238,9 +259,13 @@ layout_index = html.Div([
                                    'height':50,'width':200,
                                'font-family':'minion'}),
             ],
-            style={'display':'inline-block','margin-left':50}),
+            style={'display':'inline-block','margin-left':100}),
         
-        html.Div([html.H3('Reports',
+        html.Div([
+            html.Img(src='data:image/png;base64,{}'.format(report_encoded_image.decode()),
+                     style={'width':100}),
+            html.Br(),
+            html.H3('Reports',
                     style={'color':'rgb(0,123,255)','textAlign':'center',
                            'font-family':'avenir'}),
             html.Button(
@@ -269,7 +294,7 @@ layout_index = html.Div([
             html.Br(),
             
             ],
-                 style={'display':'inline-block','margin-left':50}),
+                 style={'display':'inline-block','margin-left':100}),
 
 
                       ],style={'textAlign':'center'}),
@@ -1300,7 +1325,9 @@ app.validation_layout = html.Div([
     [Input('url', 'pathname'),]
      )
 def display_page(pathname):
-    if pathname == "/notification":
+    if pathname == "/dummy":
+        return layout_index
+    elif pathname == "/notification":
         return notification_page
     elif pathname == "/order":
         return order_page
@@ -1325,6 +1352,7 @@ def display_page(pathname):
 @app.callback(
     Output('url','pathname'),
     [
+     Input('dummy-button','n_clicks'),
      Input('notif-button','n_clicks'),
      Input('order-button','n_clicks'),
      Input('user-button','n_clicks'),
@@ -1335,12 +1363,14 @@ def display_page(pathname):
      Input('report2-button','n_clicks'),
      ]
     )
-def menu_output(notif_button,order_button,user_button,equi_button,
+def menu_output(dummy_button,notif_button,order_button,user_button,equi_button,
                 damage_button,location_button,report1_button,report2_button):   
     ctx = dash.callback_context
     if ctx.triggered:
        eventid = ctx.triggered[0]['prop_id'].split('.')[0]
-       if eventid == 'notif-button':
+       if eventid == 'dummy-button':
+           path = '/'
+       elif eventid == 'notif-button':
            path = '/notification'
        elif eventid == 'order-button':
            path = '/order'
